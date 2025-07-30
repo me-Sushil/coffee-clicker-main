e929862621a5fc7532970937020f3cf00a3b9e82
me-Sushil <girbahadurbk123@gmail.com>
2025-07-31 01:45:59 +0545

    update code by removing unwanted code

--- script.js
+++ script.js
@@ -130,43 +130,19 @@ function tick(data) {
 
 }
 
-/*************************
- *  Start your engines!
- *************************/
-
-// You don't need to edit any of the code below
-// But it is worth reading so you know what it does!
-
-// So far we've just defined some functions; we haven't actually
-// called any of them. Now it's time to get things moving.
-
-// We'll begin with a check to see if we're in a web browser; if we're just running this code in node for purposes of testing, we don't want to 'start the engines'.
-
-// How does this check work? Node gives us access to a global variable /// called `process`, but this variable is undefined in the browser. So,
-// we can see if we're in node by checking to see if `process` exists.
 if (typeof process === "undefined") {
-  // Get starting data from the window object
-  // (This comes from data.js)
   const data = window.data;
 
-  // Add an event listener to the giant coffee emoji
   const bigCoffee = document.getElementById("big_coffee");
   bigCoffee.addEventListener("click", () => clickCoffee(data));
 
-  // Add an event listener to the container that holds all of the producers
-  // Pass in the browser event and our data object to the event listener
   const producerContainer = document.getElementById("producer_container");
   producerContainer.addEventListener("click", (event) => {
     buyButtonClick(event, data);
   });
 
-  // Call the tick function passing in the data object once per second
   setInterval(() => tick(data), 1000);
 }
-// Meanwhile, if we aren't in a browser and are instead in node
-// we'll need to exports the code written here so we can import and
-// Don't worry if it's not clear exactly what's going on here;
-// We just need this to run the tests in Mocha.
 else if (process) {
   module.exports = {
     updateCoffeeView,
